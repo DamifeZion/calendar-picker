300638d83765d3e314b9c5dcd3228bdbfbe3825d
DamifeZion <damifeolaleye@gmail.com>
2025-01-29 12:52:32 +0100

    fix: Removed unnecessary inputs and components

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,12 @@
 <script setup lang="ts">
-import ComplexDatePicker from "@/components/date-picker/ComplexDatePicker.vue";
-import SimpleDatePicker from "@/components/date-picker/SimpleDatePicker.vue";
+import DatePicker from "@/components/DatePicker.vue";
 </script>
 
 <template>
    <section class="grid items-center py-16 gap-x-6 gap-y-4 md:grid-cols-2">
       <div class="space-y-2">
-         <label class="text-lg font-medium">Simple Date Range</label>
-         <SimpleDatePicker />
-      </div>
-
-      <div class="space-y-2">
-         <label class="text-lg font-medium"> Advanced Date Range</label>
-         <ComplexDatePicker class="max-w-none" />
+         <label class="text-lg font-medium">Date Range</label>
+         <DatePicker />
       </div>
    </section>
 </template>
